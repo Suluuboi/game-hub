f67736ee49b2b0af90837b27b4ecf4a24e7f1f71
Home PC <hansmbangu@gmail.com>
2023-06-11 22:16:46 +0200

    Displaying genre images

--- src/components/side-filter/GenreList.tsx
+++ src/components/side-filter/GenreList.tsx
@@ -1,4 +1,7 @@
+import { HStack, List, ListItem, Text } from "@chakra-ui/layout";
 import useGenres from "../../hooks/useGenres";
+import { Image } from "@chakra-ui/image";
+import getCropedImageUrl from "../../services/image-url";
 
 interface Props {}
 
@@ -6,11 +9,20 @@ export default function GenreList({}: Props) {
   const { data: genres, error, loading } = useGenres();
   return (
     <>
-      <ul>
+      <List>
         {genres.map((g) => (
-          <li key={g.id}>{g.name}</li>
+          <ListItem key={g.id} paddingY={"5px"}>
+            <HStack>
+              <Image
+                boxSize="32px"
+                src={getCropedImageUrl(g.image_background).toString()}
+                borderRadius={8}
+              />
+              <Text fontSize={"lg"}>{g.name}</Text>
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
