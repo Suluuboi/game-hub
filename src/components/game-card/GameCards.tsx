8ac74e0aa78961847de700395b8315e852cd6703
Home PC <hansmbangu@gmail.com>
2023-06-12 23:34:25 +0200

    Clean Up Game Card

--- src/components/game-card/GameCards.tsx
+++ src/components/game-card/GameCards.tsx
@@ -14,7 +14,9 @@ export default function GameCards({ game }: Props) {
     <Card>
       <Image src={getCropedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <Heading fontSize={"2xl"} marginBottom={2}>
+          {game.name}
+        </Heading>
         <HStack justifyContent={"space-between"}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
