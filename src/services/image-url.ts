07a0a58036ae5a8f54da6210e814105b2a9ea5c6
Hans Mbanugu[work-PC] <HansMbangu@gmail.com>
2023-06-12 18:06:20 +0200

    handel games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,10 @@
+import noImage from '../assets/no-image-placeholder.webp'
 /***
  * add 'crop/600/400/' inbeteen the 'media/' string in the text provided
  * the https://api.rawg.io/api api alow users to get croped images
  */
 export default function getCropedImageUrl(url: string) {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
